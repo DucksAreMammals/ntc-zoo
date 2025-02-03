749defc7d265922678a6d45c9b86fa2a4d93bccb
Lucas <bdlucas@students.ntc.edu>
2025-02-03 01:55:50 -0600

    initial commit

---
+++ zoo/zoo.js
@@ -0,0 +1,10 @@
+let elephantCount = 2;
+let tigerCount = 4;
+let pandaCount = 1;
+
+let animalCount = elephantCount + tigerCount + pandaCount;
+console.log(`There are ${animalCount} animals in the NTC Zoo.`);
+
+let isZooOpen = true;
+let zooStatus = isZooOpen ? "open" : "closed";
+console.log(`The zoo is ${zooStatus}!`);
